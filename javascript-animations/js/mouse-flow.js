935dd6d8180082bfe3be65ead1ebe0599d1a11bc
Md Somrat Akbor <somrat876@gmail.com>
2023-11-30 16:53:13 +0600

    animation implement

--- javascript-animations/js/mouse-flow.js
+++ javascript-animations/js/mouse-flow.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById('cw');
 const context = canvas.getContext('2d');
-context.globarAlpha = 0.5;
+context.globalAlpha = 0.5;
 
 const cursor = {
     x: innerWidth / 2,
@@ -9,44 +9,48 @@ const cursor = {
 
 let particlesArray = [];
 
-generateParticles = (101);
+generateParticles(101);
 setSize();
 anim();
-addEventListener('mouseover', (e)=>{
+addEventListener('mousemove', (e) => {
     cursor.x = e.clientX;
     cursor.y = e.clientY;
 })
-addEventListener('touchmove', (e)=>{
+addEventListener('touchmove', (e) => {
     cursor.x = e.touches[0].clientX;
     cursor.y = e.touches[0].clientY;
-}, {passive: false})
-addEventListener('resize', ()=>setSize());
+}, { passive: false })
+addEventListener('resize', () => setSize());
 
-function generateParticles(n){
-    for(let i = 0; i < n; i++){
+function generateParticles(n) {
+    particlesArray = [];
+    for (let i = 0; i < n; i++) {
         particlesArray.push(new Particle(
             innerWidth / 2,
-            innerHeight/ 2,
+            innerHeight / 2,
             4,
-            gnerateColor(),
+            generateColor(),
             0.02
         ));
     }
 }
-function gnerateColor(){
+
+function generateColor() {
     let hexset = '0123456789ABCDEF';
     let finalHexString = "#";
-    for(let i=0; i<6; i++){
+    for (let i = 0; i < 6; i++) {
         finalHexString += hexset[Math.floor(Math.random() * 16)];
     }
     return finalHexString;
 }
-function setSize(){
+
+function setSize() {
     canvas.height = innerHeight;
     canvas.width = innerWidth;
+    generateParticles(101);
 }
 
-function Particle(x, y, particleTrainWidth, color, rotateSpeed){
+function Particle(x, y, particleTrainWidth, color, rotateSpeed) {
     this.x = x;
     this.y = y;
     this.particleTrainWidth = particleTrainWidth;
@@ -54,7 +58,7 @@ function Particle(x, y, particleTrainWidth, color, rotateSpeed){
     this.theta = Math.random() * Math.PI * 2;
     this.rotateSpeed = rotateSpeed;
     this.t = Math.random() * Math.PI * 2;
-    this.rotate = ()=>{
+    this.rotate = () => {
         const ls = {
             x: this.x,
             y: this.y
@@ -66,12 +70,12 @@ function Particle(x, y, particleTrainWidth, color, rotateSpeed){
         context.lineWidth = this.particleTrainWidth;
         context.strokeStyle = this.color;
         context.moveTo(ls.x, ls.y);
-        this.lineto(this.x, this.y);
+        context.lineTo(this.x, this.y);
         context.stroke();
     }
-
 }
-function anim(){
+
+function anim() {
     requestAnimationFrame(anim);
     context.fillStyle = "rgba(0,0,0,0.05)";
     context.fillRect(0, 0, canvas.width, canvas.height);
